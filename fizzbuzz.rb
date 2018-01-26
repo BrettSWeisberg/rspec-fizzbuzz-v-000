4ffdef239ebff1c6dac8a2697120b2c6e1ffe68d
Brett Weisberg <weisberg.brett@gmail.com>
2018-01-26 01:25:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 require_relative './spec_helper.rb'
 spec/fizzbuzz_spec.rb
 
-def fizzbuzz(X)
-  if X % 15 == 0
+def fizzbuzz(x)
+  if x % 15 == 0
    "Fizzbuzz"
-elsif X % 3 == 0
+elsif x % 3 == 0
    "Fizz"
-elsif X % 5 == 0
+elsif x % 5 == 0
   "Buzz"
 else
   "nil"
