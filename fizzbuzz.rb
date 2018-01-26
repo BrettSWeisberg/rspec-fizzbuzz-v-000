ccf4acfa296b2c2927d7885eaf25dde3c5f669e4
Brett Weisberg <weisberg.brett@gmail.com>
2018-01-26 01:57:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(x)
   if x % 3 == 0 && x % 5 == 0
    "Fizzbuzz"
-elsif x % 3 == 0
-   "Fizz"
-elsif x % 5 == 0
-  "Buzz"
-else
-  "nil"
-end
+  elsif x % 3 == 0
+     "Fizz"
+  elsif x % 5 == 0
+    "Buzz"
+  else
+    nil
+  end
 end
